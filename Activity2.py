77f429019177953ceca66b6da8edb18d715f1ee4
20220844 <110364810+20220844@users.noreply.github.com>
2022-08-13 17:08:39 +1200

    Activity2

---
+++ Activity2.py
@@ -0,0 +1,13 @@
+
+
+#Activity 2
+month=input("Input the name of the Month:")
+if month=="January" or month=="March" or month=="May"or month=="July"or month=="August"or month=="December":
+    print("No. of days:31 days")
+elif month=="April"or month=="June"or month=="September"or month=="November":
+    print("No. of days:30 days")
+
+else: 
+    month=="Febuary"
+    print ("No. of days:28/29 days")
+     
